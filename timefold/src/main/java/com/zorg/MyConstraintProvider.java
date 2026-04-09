d4c82f08d4adf0c7e42f4e2d343b52e3a61c5971
2304666 <2304666bollen@zuyd.nl>
2026-04-09 09:40:23 +0200

    update timefold

--- timefold/src/main/java/com/zorg/MyConstraintProvider.java
+++ timefold/src/main/java/com/zorg/MyConstraintProvider.java
@@ -20,31 +20,39 @@ public class MyConstraintProvider implements ConstraintProvider {
             noOverlap(f),
             clientTimeWindow(f),
             employeeTimeWindow(f),
-            minimizeTravel(f)
+            minimizeTravel(f),
+            balanceEmployeeDays(f),
+            balanceDays(f),
+            assignEmployee(f),
+            //sameDayChain(f),
+            useAllDays(f),
+            encourageMultipleDays(f),
+            balanceEmployeeWorkload(f),
         };
     }
 
     private Constraint dogsConstraint(ConstraintFactory f) {
         return f.forEach(Visit.class)
-                .filter(v -> v.employee.dogs != -1 && v.employee.dogs < v.client.dogs)
+                .filter(v -> v.employee != null &&  v.employee.dogs != -1 && v.employee.dogs < v.client.dogs)
                 .penalize("Too many dogs", HardSoftScore.ONE_HARD);
     }
 
     private Constraint catsConstraint(ConstraintFactory f) {
         return f.forEach(Visit.class)
-                .filter(v -> v.employee.cats != -1 && v.employee.cats < v.client.cats)
+                .filter(v -> v.employee != null &&  v.employee.cats != -1 && v.employee.cats < v.client.cats)
                 .penalize("Too many cats", HardSoftScore.ONE_HARD);
     }
 
     private Constraint smokeConstraint(ConstraintFactory f) {
         return f.forEach(Visit.class)
-                .filter(v -> v.client.smokes && !v.employee.smokes)
+                .filter(v -> v.employee != null &&  v.client.smokes && !v.employee.smokes)
                 .penalize("Smoking not allowed", HardSoftScore.ONE_HARD);
     }
 
     private Constraint noOverlap(ConstraintFactory f) {
     return f.forEachUniquePair(Visit.class,
-            Joiners.equal(v -> v.employee))
+            Joiners.equal(v -> v.employee),
+            Joiners.equal(v -> v.day))
         .filter((v1, v2) -> {
             if (v1.startTime == null || v2.startTime == null) return false;
 
@@ -70,13 +78,13 @@ public class MyConstraintProvider implements ConstraintProvider {
 
             return v.startTime.isBefore(start) || v.startTime.isAfter(end);
         })
-        .penalize("Outside client window", HardSoftScore.ONE_HARD);
+        .penalize("Outside client window", HardSoftScore.ONE_SOFT);
     }
 
     private Constraint employeeTimeWindow(ConstraintFactory f) {
     return f.forEach(Visit.class)
         .filter(v -> {
-            if (v.startTime == null) return false;
+            if (v.startTime == null || v.employee == null) return false;
 
                 LocalTime start = LocalTime.parse(v.employee.timeWindowStart);
                 LocalTime end = LocalTime.parse(v.employee.timeWindowEnd);
@@ -126,4 +134,53 @@ public class MyConstraintProvider implements ConstraintProvider {
                     return (int)(dist * 100);
                 });
     }
+
+        private Constraint balanceEmployeeDays(ConstraintFactory f) {
+            return f.forEach(Visit.class)
+                    .groupBy(v -> v.employee, v -> v.day, ConstraintCollectors.count())
+                    .penalize("Too many visits for one employee on one day", HardSoftScore.ONE_SOFT,
+                        (employee, day, count) -> count > 1 ? (count-1)*10 : 0);
+        }
+
+        private Constraint balanceDays(ConstraintFactory f) {
+            return f.forEach(Visit.class)
+                .groupBy(v -> v.day, ConstraintCollectors.count())
+                .penalize("Unbalanced days", HardSoftScore.ONE_SOFT,
+                    (day, count) -> count * count);
+        }
+
+        private Constraint assignEmployee(ConstraintFactory f) {
+            return f.forEach(Visit.class)
+                .filter(v -> v.employee == null)
+                .penalize("Unassigned visit", HardSoftScore.ONE_HARD);
+        }
+
+        private Constraint sameDayChain(ConstraintFactory f) {
+            return f.forEach(Visit.class)
+                .filter(v -> v.previousVisit != null &&
+                            v.day != null &&
+                            v.previousVisit.day != null &&
+                            !v.day.equals(v.previousVisit.day))
+                .penalize("Chain across days", HardSoftScore.ONE_HARD);
+        }
+
+        private Constraint useAllDays(ConstraintFactory f) {
+            return f.forEach(Visit.class)
+                .groupBy(v -> v.day, ConstraintCollectors.count())
+                .penalize("Too many visits on one day", HardSoftScore.ONE_SOFT,
+                    (day, count) -> count * count);
+        }
+
+        private Constraint encourageMultipleDays(ConstraintFactory f) {
+            return f.forEach(Visit.class)
+                .groupBy(v -> v.day, ConstraintCollectors.count())
+                .reward("Use more days", HardSoftScore.ONE_SOFT);
+        }
+
+        private Constraint balanceEmployeeWorkload(ConstraintFactory f) {
+            return f.forEach(Visit.class)
+                    .groupBy(v -> v.employee, ConstraintCollectors.count())
+                    .penalize("Workload per employee", HardSoftScore.ONE_SOFT,
+                        (employee, count) -> (count - 4) * (count - 4)); // streef naar 4 per employee
+        }
 }
